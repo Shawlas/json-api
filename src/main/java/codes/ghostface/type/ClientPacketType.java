271af377830a3a19b0cc83e407c22b121d75191b
ghost_face_dev <henriquedev6@gmail.com>
2024-09-05 17:35:46 -0300

    redoing types

--- src/main/java/codes/ghostface/type/ClientPacketType.java
+++ src/main/java/codes/ghostface/type/ClientPacketType.java
@@ -3,33 +3,12 @@ package codes.ghostface.type;
 import org.jetbrains.annotations.NotNull;
 
 public enum ClientPacketType {
-    HTTP_REQUEST("HTTP Request"),
-    HTTP_DATA("HTTP Data"),
-    FTP_COMMAND("FTP Command"),
-    FTP_DATA("FTP Data"),
-    SMTP_COMMAND("SMTP Command"),
-    SMTP_DATA("SMTP Data"),
-    POP3_COMMAND("POP3 Command"),
-    IMAP_COMMAND("IMAP Command"),
-    WEBSOCKET_HANDSHAKE("WebSocket Handshake"),
-    WEBSOCKET_MESSAGE("WebSocket Message"),
-    DNS_QUERY("DNS Query"),
-    SSH_CONNECTION_REQUEST("SSH Connection Request"),
-    SSH_COMMAND_EXECUTION("SSH Command Execution"),
-    SSH_DATA_TRANSFER("SSH Data Transfer"),
-    TCP_SEGMENT("TCP Segment"),
-    TCP_SYN("TCP SYN"),
-    TCP_ACK("TCP ACK"),
-    TCP_FIN("TCP FIN"),
-    UDP_DATAGRAM("UDP Datagram"),
-    IP_DATAGRAM("IP Datagram"),
-    ICMP_ECHO_REQUEST("ICMP Echo Request"),
-    ETHERNET_FRAME("Ethernet Frame"),
-    ARP_REQUEST("ARP Request"),
-    ARP_REPLY("ARP Reply"),
-    DHCP_DISCOVER("DHCP Discover"),
-    DHCP_REQUEST("DHCP Request"),
-    NTP_REQUEST("NTP Request");
+    CONNECTION("connection"),
+    MESSAGE("message"),
+    AUTHENTICATION("authentication"),
+    DATA("data"),
+    COMMAND("command"),
+    STATE("state");
 
     private final @NotNull String description;
 
--- src/main/java/codes/ghostface/type/ServerPacketType.java
+++ src/main/java/codes/ghostface/type/ServerPacketType.java
@@ -3,28 +3,11 @@ package codes.ghostface.type;
 import org.jetbrains.annotations.NotNull;
 
 public enum ServerPacketType {
-    FTP_DATA("FTP Data"),
-    POP3_DATA("POP3 Data"),
-    IMAP_DATA("IMAP Data"),
-    WEBSOCKET_MESSAGE("WebSocket Message"),
-    DNS_RESPONSE("DNS Response"),
-    SSH_DATA_TRANSFER("SSH Data Transfer"),
-    TCP_SEGMENT("TCP Segment"),
-    TCP_ACK("TCP ACK"),
-    TCP_FIN("TCP FIN"),
-    UDP_DATAGRAM("UDP Datagram"),
-    IP_DATAGRAM("IP Datagram"),
-    ICMP_ECHO_REPLY("ICMP Echo Reply"),
-    ICMP_DESTINATION_UNREACHABLE("ICMP Destination Unreachable"),
-    ICMP_TIME_EXCEEDED("ICMP Time Exceeded"),
-    ETHERNET_FRAME("Ethernet Frame"),
-    ARP_REQUEST("ARP Request"),
-    ARP_REPLY("ARP Reply"),
-    DHCP_OFFER("DHCP Offer"),
-    DHCP_ACK("DHCP ACK"),
-    NTP_RESPONSE("NTP Response"),
-    HTTP_RESPONSE("HTTP Response"),
-    HTTP_DATA("HTTP Data");
+    DATA("data"),
+    MESSAGE("message"),
+    RESPONSE("response"),
+    ERROR("error"),
+    STATE("state");
 
     private final String description;
 
