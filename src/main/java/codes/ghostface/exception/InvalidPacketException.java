f1786e5a5e2e994a3baca588bc91c85ccc774469
ghost_face_dev <henriquedev6@gmail.com>
2024-09-06 15:20:30 -0300

    improved code

---
+++ src/main/java/codes/ghostface/exception/InvalidPacketException.java
@@ -0,0 +1,7 @@
+package codes.ghostface.exception;
+
+public class InvalidPacketException extends Exception {
+    public InvalidPacketException(String message) {
+        super(message);
+    }
+}
--- src/main/java/codes/ghostface/impl/utils/PacketUtilsImpl.java
+++ src/main/java/codes/ghostface/impl/utils/PacketUtilsImpl.java
@@ -81,4 +81,117 @@ public final class PacketUtilsImpl implements PacketUtils {
         }
     }
 
+    // Classes
+
+    private static final class ClientPacketHandlerImpl implements ClientPacketHandler {
+
+        private final @NotNull ClientPacket packet;
+        private final @NotNull ClientCheckers checkers;
+
+        public ClientPacketHandlerImpl(@NotNull ClientPacket packet) {
+            this.packet = packet;
+            this.checkers = new ClientCheckersImpl();
+        }
+
+        @Override
+        public @NotNull ClientPacket getPacket() {
+            return packet;
+        }
+
+        @Override
+        public @NotNull ClientCheckers getCheckers() {
+            return checkers;
+        }
+
+        // Classes
+
+        private final class ClientCheckersImpl implements ClientCheckers {
+
+            @Override
+            public boolean isAuthentication() {
+                return packet instanceof AuthenticationPacket;
+            }
+
+            @Override
+            public boolean isRegister() {
+                return packet instanceof RegisterPacket;
+            }
+
+            @Override
+            public boolean isMessage() {
+                return packet instanceof MessagePacket;
+            }
+
+            @Override
+            public boolean isConnection() {
+                return false;
+            }
+
+            @Override
+            public boolean isData() {
+                return false;
+            }
+
+            @Override
+            public boolean isState() {
+                return false;
+            }
+
+            @Override
+            public boolean isCommand() {
+                return false;
+            }
+        }
+    }
+
+    private static final class ServerPacketHandlerImpl implements ServerPacketHandler {
+
+        private final @NotNull ServerPacket packet;
+        private final @NotNull ServerCheckers checkers;
+
+        public ServerPacketHandlerImpl(@NotNull ServerPacket packet) {
+            this.packet = packet;
+            this.checkers = new ServerCheckersImpl();
+        }
+
+        @Override
+        public @NotNull ServerPacket getPacket() {
+            return packet;
+        }
+
+        @Override
+        public @NotNull ServerCheckers getCheckers() {
+            return checkers;
+        }
+
+        // Classes
+
+        private final class ServerCheckersImpl implements ServerCheckers {
+
+            @Override
+            public boolean isResponse() {
+                return packet instanceof ServerResponsePacket;
+            }
+
+            @Override
+            public boolean isError() {
+                return packet instanceof ServerErrorPacket;
+            }
+
+            @Override
+            public boolean isData() {
+                return false;
+            }
+
+            @Override
+            public boolean isMessage() {
+                return false;
+            }
+
+            @Override
+            public boolean isState() {
+                return false;
+            }
+        }
+    }
 }
